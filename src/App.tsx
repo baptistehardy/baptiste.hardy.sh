1a4ef5c37b13b98e386f3aa7c99611a9b78a666e
Baptiste Hardy <baptiste@hardy.sh>
2024-09-02 22:20:11 +0200

    Added some content (wip)

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,66 @@
 function App() {
-  return (
-    <>
-      <h1 className="text-5xl text-white font-semibold">baptiste.hardy.sh</h1>
-    </>
-  )
+    return (
+        <>
+            <h1 className="text-5xl text-white font-semibold my-4">Baptiste Hardy</h1>
+            <p>
+                Développeur full-stack basé à Chartres
+                (Centre-Val de Loire)
+                <br/>
+                Je possède les compétences pour développer des
+                services et produits du cahier des charges, de la conception
+                et du design, jusqu'au déploiement.
+            </p>
+            <h2 className="text-3xl text-bold my-4">Expérience</h2>
+            <ul>
+                <li>BTS SIO</li>
+                <li>Licence</li>
+                <li>C'CIN (2019-2024)</li>
+            </ul>
+            <h2 className="text-3xl text-bold my-4">Skills</h2>
+            <h3 className="text-xl text-semibold my-2">Langages</h3>
+            <ul>
+                <li>HTML5/CSS3</li>
+                <li>JavaScript</li>
+                <li>TypeScript</li>
+                <li>PHP</li>
+                <li>Go</li>
+                <li>Nix</li>
+                <li>(Rust)</li>
+            </ul>
+            <h3 className="text-xl text-semibold my-4">Frameworks</h3>
+            <ul>
+                <li>Symfony</li>
+                <li>(React)</li>
+                <li>Flutter</li>
+                <li>etc...</li>
+            </ul>
+            <h3 className="text-xl text-semibold my-4">Libraires</h3>
+            <ul>
+                <li>React</li>
+                <li>etc...</li>
+            </ul>
+            <h3 className="text-xl text-semibold my-4">Software</h3>
+            <ul>
+                <li>Office Suite</li>
+                <li>Windows</li>
+                <li>Linux</li>
+                <li>Docker</li>
+                <li>etc...</li>
+            </ul>
+            <h2 className="text-3xl text-bold my-4">Projets</h2>
+            <ul>
+                <li>prythm</li>
+                <li></li>
+            </ul>
+            <h2 className="text-3xl text-bold my-4">Passions/hobbies</h2>
+            <ul>
+                <li>Photographie</li>
+                <li>Vélo</li>
+                <li>Homelabbing/self-hosting</li>
+                <li>Fan d'animation</li>
+            </ul>
+        </>
+    )
 }
 
 export default App
